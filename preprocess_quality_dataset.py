586ce22da29323e0d658d599e72af42498b5c5b6
WALL-AI <gaojing850063636@gmail.com>
2024-09-30 15:19:08 +0800

    feat:add evol_instruct methond build sft datasets

--- preprocess_quality_dataset.py
+++ preprocess_quality_dataset.py
@@ -11,7 +11,7 @@ import seaborn as sns
 
 from entities.image_entity import ImageTableProcess
 from llm import LLMApi
-from prompt import QUALITY_MAIN_STRUCTURE_PROMOPT_LABEL, QUALITY_MAIN_STRUCTURE_PROMOPT_LABEL_UPDATE, QUALITY_MAIN_STRUCTURE_RISK_LABEL, QUALITY_MAIN_STRUCTURE_RISK_PROMPT
+from prompt.prompt import QUALITY_MAIN_STRUCTURE_PROMOPT_LABEL, QUALITY_MAIN_STRUCTURE_PROMOPT_LABEL_UPDATE, QUALITY_MAIN_STRUCTURE_RISK_LABEL, QUALITY_MAIN_STRUCTURE_RISK_PROMPT
 from utils.helper import download_image, load_images_from_folder, write_json_file
 font = FontProperties(fname=r'/home/dataset-s3-0/gaojing/llm/easy-rag/data/SimHei.ttf')
 
