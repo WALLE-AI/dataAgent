ef22f89b1c58d65705f2394ca5f659913bc05429
WALL-AI <gaojing850063636@gmail.com>
2024-10-12 18:33:40 +0800

    feat:retrieval service

---
+++ embedding/datasource/retrieval_service.py
@@ -0,0 +1,173 @@
+import threading
+from typing import Optional
+
+import loguru
+
+from embedding.datasource.vector_factory import Vector
+from entities.retrieval_methods import RetrievalMethod
+from models.reranker import RankerApi
+
+
+default_retrieval_model = {
+    "search_method": RetrievalMethod.SEMANTIC_SEARCH.value,
+    "reranking_enable": False,
+    "reranking_model": {"reranking_provider_name": "", "reranking_model_name": ""},
+    "top_k": 2,
+    "score_threshold_enabled": False,
+}
+
+
+class RetrievalService:
+    @classmethod
+    def retrieve(
+        cls,
+        retrieval_method: str,
+        docs_index: str,
+        query: str,
+        top_k: int,
+        score_threshold: Optional[float] = 0.0,
+        reranking_model: Optional[bool] = False,
+    ):
+        all_documents = []
+        threads = []
+        exceptions = []
+        # retrieval_model source with keyword 可以参考ragflow中tokenier的方案
+        # if retrieval_method == "keyword_search":
+        #     keyword_thread = threading.Thread(
+        #         target=RetrievalService.keyword_search,
+        #         kwargs={
+        #             "dataset_id": dataset_id,
+        #             "query": query,
+        #             "top_k": top_k,
+        #             "all_documents": all_documents,
+        #             "exceptions": exceptions,
+        #         },
+        #     )
+        #     threads.append(keyword_thread)
+        #     keyword_thread.start()
+        # retrieval_model source with semantic
+        if RetrievalMethod.is_support_semantic_search(retrieval_method):
+            embedding_thread = threading.Thread(
+                target=RetrievalService.embedding_search,
+                kwargs={
+                    "docs_index": docs_index,
+                    "query": query,
+                    "top_k": top_k,
+                    "score_threshold": score_threshold,
+                    "reranking_model": reranking_model,
+                    "all_documents": all_documents,
+                    "exceptions":exceptions
+                },
+            )
+            threads.append(embedding_thread)
+            embedding_thread.start()
+
+        # retrieval source with full text
+        if RetrievalMethod.is_support_fulltext_search(retrieval_method):
+            full_text_index_thread = threading.Thread(
+                target=RetrievalService.full_text_index_search,
+                kwargs={
+                    "docs_index": docs_index,
+                    "query": query,
+                    "top_k": top_k,
+                    "reranking_model": reranking_model,
+                    "all_documents": all_documents,
+                    "exceptions":exceptions
+                },
+            )
+            threads.append(full_text_index_thread)
+            full_text_index_thread.start()
+
+        for thread in threads:
+            thread.join()
+
+        if exceptions:
+            exception_message = ";\n".join(exceptions)
+            raise Exception(exception_message)
+
+        if retrieval_method == RetrievalMethod.HYBRID_SEARCH.value:
+            pass
+        return all_documents
+
+    # @classmethod
+    # def external_retrieve(cls, dataset_id: str, query: str, external_retrieval_model: Optional[dict] = None):
+    #     dataset = db.session.query(Dataset).filter(Dataset.id == dataset_id).first()
+    #     if not dataset:
+    #         return []
+    #     all_documents = ExternalDatasetService.fetch_external_knowledge_retrieval(
+    #         dataset.tenant_id, dataset_id, query, external_retrieval_model
+    #     )
+    #     return all_documents
+
+    # @classmethod
+    ##可以使用jieba切好关键字或者tokenier的方式
+    # def keyword_search(
+    #     cls, dataset_id: str, query: str, top_k: int, all_documents: list, exceptions: list
+    # ):
+    #     try:
+    #         dataset = db.session.query(Dataset).filter(Dataset.id == dataset_id).first()
+
+    #         keyword = Keyword(dataset=dataset)
+
+    #         documents = keyword.search(cls.escape_query_for_search(query), top_k=top_k)
+    #         all_documents.extend(documents)
+    #     except Exception as e:
+    #         exceptions.append(str(e))
+
+    @classmethod
+    def embedding_search(
+        cls,
+        docs_index: str,
+        query: str,
+        top_k: int,
+        score_threshold: Optional[float],
+        reranking_model: bool,
+        all_documents: list,
+        exceptions:list
+    ):
+        try:
+            vector = Vector(docs_index)
+            documents = vector.search_by_vector(
+                    cls.escape_query_for_search(query),
+                    search_type="similarity_score_threshold",
+                    top_k=top_k,
+                    score_threshold=score_threshold,
+            )
+
+            if documents:
+                if reranking_model:
+                    reranker_reponse = RankerApi.reranker_documents(query,documents)
+                    all_documents.append(reranker_reponse)
+                else:
+                    all_documents.extend(documents)
+        except Exception as e:
+            loguru.logger.info(f"embedding search error:{e}")
+            exceptions.append(str(e))
+    @classmethod
+    def full_text_index_search(
+        cls,
+        docs_index: str,
+        query: str,
+        top_k: int,
+        reranking_model: Optional[dict],
+        all_documents: list,
+        exceptions:list
+    ):
+        try:
+
+            vector_processor = Vector(docs_index)
+
+            documents = vector_processor.search_by_full_text(cls.escape_query_for_search(query), top_k=top_k)
+            if documents:
+                if reranking_model:
+                    reranker_reponse = RankerApi.reranker_documents(query,documents)
+                    all_documents.append(reranker_reponse)
+                else:
+                    all_documents.extend(documents)
+        except Exception as e:
+            loguru.logger.info(f"embedding full search error:{e}")
+            exceptions.append(str(e))
+
+    @staticmethod
+    def escape_query_for_search(query: str) -> str:
+        return query.replace('"', '\\"')
--- test_chroma_vdb.py
+++ test_chroma_vdb.py
@@ -2,6 +2,7 @@ import uuid
 from dotenv import load_dotenv
 import loguru
 
+from embedding.datasource.retrieval_service import RetrievalService
 from embedding.datasource.vector_factory import Vector
 from entities.document import Document
 from models.embedding import EmbeddingApi
@@ -17,15 +18,24 @@ if __name__ == "__main__":
     doc_id = str(uuid.uuid4())
     collection_name =  "Vector_index_"+doc_id+"_Node" 
     collection_name =  "Vector_index_96974a26_8dcd_4500_b2c3_4259a61f313a_Node_test"
-    vec = Vector(collection_name)
-    # doc_res = ["你是谁","你能够做什么","建筑大模型解决质量安全隐患问题","什么是建筑大模型，该模型具备该领域什么样特性"]
-    # documents=[]
-    # for doc in doc_res:
-    #     metadata = {"doc_id":str(uuid.uuid4())}
-    #     documents.append(Document(page_content=doc,metadata=metadata))
-    # vec.create(documents)
+    # vec = Vector(collection_name)
+    # # doc_res = ["你是谁","你能够做什么","建筑大模型解决质量安全隐患问题","什么是建筑大模型，该模型具备该领域什么样特性"]
+    # # documents=[]
+    # # for doc in doc_res:
+    # #     metadata = {"doc_id":str(uuid.uuid4())}
+    # #     documents.append(Document(page_content=doc,metadata=metadata))
+    # # vec.create(documents)
     query = "漏筋"
-    response = vec.search_by_vector(query,score_threshold=0.2)
-    reranker_reponse = RankerApi.reranker_documents(query,response)
-    loguru.logger.info(f"reponse:{reranker_reponse}")
+    top_k = 5
+    score = 0.5
+    reranking_model = True
+    all_documents = []
+    retravial = "semantic_search"
+    # response = vec.search_by_vector(query,score_threshold=0.2)
+    # reranker_reponse = RankerApi.reranker_documents(query,response)
+    # loguru.logger.info(f"reponse:{reranker_reponse}")
+    all_documents = RetrievalService.retrieve(retrieval_method=retravial,docs_index=collection_name,query=query,
+                              top_k=top_k,score_threshold=score,
+                              reranking_model=reranking_model)
+    loguru.logger.info(f"reponse:{all_documents}")
     
